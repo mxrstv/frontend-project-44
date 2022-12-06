032a9b25a69fcf06772304dbc0c99a25c084fae4
Max Erastov <max.erastov@gmail.com>
2022-12-06 13:36:41 +0300

    Add eslint and CodeClimate

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const userGreeting = () => {
+export default () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-}
-
+};
